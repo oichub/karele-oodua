2bf6f95e61065e26681dd557244429b57bb8e572
Ibikunle Johnson <johnson.ibikunle@talosmart.com>
2021-08-12 13:36:23 +0100

    commit after setting welcome email using mailtrap

--- resources/views/welcome_email.blade.php
+++ resources/views/welcome_email.blade.php
@@ -3,10 +3,12 @@ Welcome to Karale Oodua Tv,<br><br>
 Here are your registration details: <br><br>
 
 <p>Name: {{$name}}</p>
+<p>Email Address: {{$email}}</p>
 <p>Phone Number: {{$phone}}</p>
 <p>Country: {{$country}}</p>
 <p>Date of birth: {{$dob}}</p>
-<p>Email Address: {{$email}}</p><br>
+<p>Password: provided-password</p><br>
+
 
 
 Thank You,<br>
